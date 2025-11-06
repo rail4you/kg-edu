44cff495c3be41cd26a117a2b7d91ac9179eb962
bai <rail4you@gmail.com>
2025-11-06 08:07:24 +0800

    add code

--- backend/kg_edu/lib/kg_edu/release.ex
+++ backend/kg_edu/lib/kg_edu/release.ex
@@ -1,10 +1,8 @@
 defmodule KgEdu.Release do
   @moduledoc """
-  Used for executing DB release tasks when run in production without Mix
-  installed.
+Tasks that need to be executed in the released application (because mix is not present in releases).
   """
   @app :kg_edu
-
   def migrate do
     load_app()
 
@@ -13,18 +11,73 @@ defmodule KgEdu.Release do
     end
   end
 
+  # only needed if you are using postgres multitenancy
+  def migrate_tenants do
+    load_app()
+
+    for repo <- repos() do
+      path = Ecto.Migrator.migrations_path(repo, "tenant_migrations")
+      # This may be different for you if you are not using the default tenant migrations
+
+      {:ok, _, _} =
+        Ecto.Migrator.with_repo(
+          repo,
+          fn repo ->
+            for tenant <- repo.all_tenants() do
+              Ecto.Migrator.run(repo, path, :up, all: true, prefix: tenant)
+            end
+          end
+        )
+    end
+  end
+
+  # only needed if you are using postgres multitenancy
+  def migrate_all do
+    load_app()
+    migrate()
+    migrate_tenants()
+  end
+
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  # only needed if you are using postgres multitenancy
+  def rollback_tenants(repo, version) do
+    load_app()
+
+    path = Ecto.Migrator.migrations_path(repo, "tenant_migrations")
+    # This may be different for you if you are not using the default tenant migrations
+
+    for tenant <- repo.all_tenants() do
+      {:ok, _, _} =
+        Ecto.Migrator.with_repo(
+          repo,
+          &Ecto.Migrator.run(&1, path, :down,
+            to: version,
+            prefix: tenant
+          )
+        )
+    end
+  end
+
   defp repos do
-    Application.fetch_env!(@app, :ecto_repos)
+    domains()
+    |> Enum.flat_map(fn domain ->
+      domain
+      |> Ash.Domain.Info.resources()
+      |> Enum.map(&AshPostgres.DataLayer.Info.repo/1)
+      |> Enum.reject(&is_nil/1)
+    end)
+    |> Enum.uniq()
+  end
+
+  defp domains do
+    Application.fetch_env!(@app, :ash_domains)
   end
 
   defp load_app do
-    # Many platforms require SSL when connecting to the database
-    Application.ensure_all_started(:ssl)
-    Application.ensure_loaded(@app)
+    Application.load(@app)
   end
 end
