92c1e92ec8b56da62662a591287acea3fc7a9a6c
bai <rail4you@gmail.com>
2026-05-22 11:31:13 +0800

    feat: 添加课程体系文档表和字段迁移

---
+++ backend/kg_edu/priv/repo/tenant_migrations/20260522000000_add_file_url_and_markdown_to_curriculum_designs.exs
@@ -0,0 +1,13 @@
+defmodule KgEdu.Repo.TenantMigrations.AddFileUrlAndMarkdownToCurriculumDesigns do
+  @moduledoc """
+  Add file_url and markdown_content to curriculum_designs table.
+  """
+  use Ecto.Migration
+
+  def change do
+    alter table(:curriculum_designs, prefix: prefix()) do
+      add :file_url, :string
+      add :markdown_content, :text
+    end
+  end
+end
